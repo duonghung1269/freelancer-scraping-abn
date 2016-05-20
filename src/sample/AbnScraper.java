789e754e2b6e5832e57683af98eea89f1c459b64
Dang Duong Hung <hungduong.dang@cimb.com>
2016-05-20 14:30:42 +0800

    - add timeout abn to reproceed

--- src/sample/AbnScraper.java
+++ src/sample/AbnScraper.java
@@ -16,6 +16,7 @@ import model.AbnCollection;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -27,6 +28,7 @@ public class AbnScraper extends AbstractScraper {
 	private static final int MAXIMUM_RECORDS_PARSED_PER_FILE = 300;
 	private static final String INPUT_PATH = "src/input/";
 	private static final String OUTPUT_PATH = "src/output/";
+	private static final String TIMEOUT_URL = "TIME_OUT";
 
 	public AbnScraper() {
 		super();
@@ -61,45 +63,56 @@ public class AbnScraper extends AbstractScraper {
 
 	public AbnCollection scrap(List<Abn> list) {
 		AbnCollection abnCollection = new AbnCollection();
+		AbnCollection timeOutAbnCollection = new AbnCollection();
 		int fileIndex = 1;
 		for (int i = 0; i < list.size(); i++) {
-			WebElement abnTextfield = webDriver
-					.findElement(By
-							.id("ctl00_TemplateBody_WebPartManager1_gwpciNewQueryMenuCommon_ciNewQueryMenuCommon_ResultsGrid_Sheet0_Input1_TextBox1"));
 			Abn abn = list.get(i);
-			abnTextfield.clear();
-			abnTextfield.sendKeys(abn.getAbn());
-			abnTextfield.sendKeys(Keys.ENTER);
-
-			WebDriverWait wait = new WebDriverWait(webDriver,
-					DEFAULT_FIND_ABN_TIMEOUT);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By
-					.xpath("//table[@class='rgMasterTable']")));
-			String url = webDriver.findElements(
-					By.xpath("//table[@class='rgMasterTable']/tbody/tr//a"))
-					.isEmpty() ? ""
-					: webDriver
-							.findElements(
-									By.xpath("//table[@class='rgMasterTable']/tbody/tr//a"))
-							.get(0).getAttribute("href");
-			abn.setUrl(url);
-			abnCollection.getAbns().add(abn);
-			System.out.println(i);
-			
-			if (i % MAXIMUM_RECORDS_PARSED_PER_FILE == 0) {
-				marshalToXML(abnCollection, "first_part_parse_" + fileIndex + ".xml");
-				abnCollection.getAbns().clear();
-				System.out.println("Parsed file index " + fileIndex);
-				fileIndex++;
+			try {
 				
-				// sleep for 10 seconds
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+				WebElement abnTextfield = webDriver
+						.findElement(By
+								.id("ctl00_TemplateBody_WebPartManager1_gwpciNewQueryMenuCommon_ciNewQueryMenuCommon_ResultsGrid_Sheet0_Input1_TextBox1"));
+				abnTextfield.clear();
+				abnTextfield.sendKeys(abn.getAbn());
+				abnTextfield.sendKeys(Keys.ENTER);
+				
+				WebDriverWait wait = new WebDriverWait(webDriver,
+						DEFAULT_FIND_ABN_TIMEOUT);
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By
+						.xpath("//table[@class='rgMasterTable']")));
+				String url = webDriver.findElements(
+						By.xpath("//table[@class='rgMasterTable']/tbody/tr//a"))
+						.isEmpty() ? ""
+								: webDriver
+								.findElements(
+										By.xpath("//table[@class='rgMasterTable']/tbody/tr//a"))
+										.get(0).getAttribute("href");
+				abn.setUrl(url);
+				abnCollection.getAbns().add(abn);
+				System.out.println(i);
+				
+				if (i % MAXIMUM_RECORDS_PARSED_PER_FILE == 0) {
+					marshalToXML(abnCollection, "first_part_parse_" + fileIndex + ".xml");
+					abnCollection.getAbns().clear();
+					System.out.println("Parsed file index " + fileIndex);
+					fileIndex++;
+					
+					// sleep for 10 seconds
+					try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
+			} catch (TimeoutException ex) {
+				ex.printStackTrace();
+				abn.setUrl(TIMEOUT_URL);
+				abnCollection.getAbns().add(abn);
+				timeOutAbnCollection.getAbns().add(abn);
 			}
 		}
+		
+		marshalToXML(timeOutAbnCollection, INPUT_PATH + "timeoutAbn.xml");
 		
 		return abnCollection;
 	}
